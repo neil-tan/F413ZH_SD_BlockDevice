1d8d1497200f7dfe2a1d24cc075f3e0c02afd545
Neil Tan <neil.c.tan@gmail.com>
2018-01-19 17:19:45 -0600

    implemented all virtual functions, fixed: byte vs number of blocks

--- F413ZH_SD_BlockDevice.cpp
+++ F413ZH_SD_BlockDevice.cpp
@@ -21,7 +21,7 @@ int F413ZH_SD_BlockDevice::deinit() {
 int F413ZH_SD_BlockDevice::program(const void *buffer, bd_addr_t addr, bd_size_t size) {
   int nBlocks = size / get_program_size();
   uint32_t *data_ptr = (uint32_t *) buffer;
-  if (BSP_SD_WriteBlocks(data_ptr, addr, nBlocks, SD_DATATIMEOUT) == MSD_OK) {
+  if (BSP_SD_WriteBlocks(data_ptr, addr / get_program_size(), nBlocks, SD_DATATIMEOUT) == MSD_OK) {
     //SD write OK
       return BD_ERROR_OK;
   } else {
@@ -33,7 +33,7 @@ int F413ZH_SD_BlockDevice::program(const void *buffer, bd_addr_t addr, bd_size_t
 int F413ZH_SD_BlockDevice::read(void *buffer, bd_addr_t addr, bd_size_t size) {
   int nBlocks = size / get_program_size();
   uint32_t *data_ptr = (uint32_t *) buffer;
-  if (BSP_SD_ReadBlocks(data_ptr, addr, nBlocks, SD_DATATIMEOUT) == MSD_OK) {
+  if (BSP_SD_ReadBlocks(data_ptr, addr / get_program_size(), nBlocks, SD_DATATIMEOUT) == MSD_OK) {
     //SD read OK
     return BD_ERROR_OK;
   } else {
@@ -42,18 +42,28 @@ int F413ZH_SD_BlockDevice::read(void *buffer, bd_addr_t addr, bd_size_t size) {
   }
 }
 
-int F413ZH_SD_BlockDevice::erase(bd_addr_t addr, bd_size_t size) {
-  if(BSP_SD_Erase(addr, size) == MSD_OK) {
-        return BD_ERROR_OK;
-    } else {
-        return BD_ERROR_DEVICE_ERROR;
-    }
+// int F413ZH_SD_BlockDevice::erase(bd_addr_t addr, bd_size_t size) {
+//   if(BSP_SD_Erase(addr, size) == MSD_OK) {
+//         return BD_ERROR_OK;
+//     } else {
+//         return BD_ERROR_DEVICE_ERROR;
+//     }
+// }
+
+bd_size_t F413ZH_SD_BlockDevice::get_program_size() const {
+  HAL_SD_CardInfoTypeDef CardInfo;
+  BSP_SD_GetCardInfo(&CardInfo);
+  return CardInfo.BlockSize;
 }
 
-bd_size_t F413ZH_SD_BlockDevice::get_program_size() {
-  return 512;
+bd_size_t F413ZH_SD_BlockDevice::get_read_size() const {
+  HAL_SD_CardInfoTypeDef CardInfo;
+  BSP_SD_GetCardInfo(&CardInfo);
+  return CardInfo.BlockSize;
 }
 
-bd_size_t F413ZH_SD_BlockDevice::get_read_size() {
-  return 512;
+bd_size_t F413ZH_SD_BlockDevice::size() const {
+  HAL_SD_CardInfoTypeDef CardInfo;
+  BSP_SD_GetCardInfo(&CardInfo);
+  return (bd_size_t)CardInfo.BlockNbr * (bd_size_t)CardInfo.BlockSize;
 }
--- F413ZH_SD_BlockDevice.h
+++ F413ZH_SD_BlockDevice.h
@@ -1,17 +1,19 @@
 #ifndef MBED_F413ZH_SD_BLOCK_DEVICE_H
 #define MBED_F413ZH_SD_BLOCK_DEVICE_H
 
+#include <mbed.h>
 #include "BlockDevice.h"
 
 class F413ZH_SD_BlockDevice : public BlockDevice {
   public:
-  int init();
-  int deinit();
-  int program(const void *buffer, bd_addr_t addr, bd_size_t size);
-  int read(void *buffer, bd_addr_t addr, bd_size_t size);
-  int erase(bd_addr_t addr, bd_size_t size);
-  bd_size_t get_program_size();
-  bd_size_t get_read_size();
+  virtual int init();
+  virtual int deinit();
+  virtual int program(const void *buffer, bd_addr_t addr, bd_size_t size);
+  virtual int read(void *buffer, bd_addr_t addr, bd_size_t size);
+  // int erase(bd_addr_t addr, bd_size_t size);
+  virtual bd_size_t get_program_size() const;
+  virtual bd_size_t get_read_size() const;
+  virtual bd_size_t size() const;
 };
 
 #endif
